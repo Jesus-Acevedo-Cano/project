4587669d821bc3e8c9b62c323b533bc2c9ef071d
Jesus Acevedo Cano <55112178+Jesus-Acevedo-Cano@users.noreply.github.com>
2020-06-10 21:59:35 -0500

    Add files via upload

---
+++ api/v1/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Flask Application """
+from models import storage
+from api.v1.views import app_views
+from os import environ, getenv
+from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Close Storage """
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+if __name__ == '__main__':
+    if getenv('HMCR_API_HOST'):
+        app.run(host=getenv('HMCR_API_HOST'),
+                port=getenv('HMCR_API_PORT'), threaded=True)
+    else:
+        app.run(host='0.0.0.0', threaded=True)  
