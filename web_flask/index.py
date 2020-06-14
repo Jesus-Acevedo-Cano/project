ea191f0cef4d28292fc7e83e2ddd0eb727bfe22a
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-06-14 22:41:15 +0000

    adding

--- web_flask/index.py
+++ web_flask/index.py
@@ -14,47 +14,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route('/register', methods=["GET", "POST"])
-def register():
-    if request.method == 'GET':
-        return render_template("register.html")
-    else:
-        name = request.form['name']
-        email = request.form['email']
-        password = request.form['password'].encode('utf-8')
-        hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
-
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users (name, email, password) VALUES (%s,%s,%s)",(name,email,hash_password,))
-        mysql.connection.commit()
-        session['name'] = request.form['name']
-        session['email'] = request.form['email']
-        return redirect(url_for('home'))
-    
-@app.route('/login',methods=["GET","POST"])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password'].encode('utf-8')
-
-        curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        curl.execute("SELECT * FROM users WHERE email=%s",(email,))
-        user = curl.fetchone()
-        curl.close()
-
-        if len(user) > 0:
-            if bcrypt.hashpw(password, user["password"].encode('utf-8')) == user["password"].encode('utf-8'):
-                session['name'] = user['name']
-                session['email'] = user['email']
-                return render_template("home.html")
-            else:
-                return "Error password and email not match"
-        else:
-            return "Error user not found"
-    else:
-        return render_template("login.html")
-
-
 @app.route('/index')
 def index():
     recipes_list = []
